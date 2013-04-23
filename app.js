b417f9db03b69810da2954a84eeb52b62ba8ea99
yojoe <joeleedel@gmail.com>
2013-04-22 23:09:33 -0400

    save history of previous tests

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 var fs = require('fs');
-var colors = require('colors');
-
-// process.stdin.resume();
-// process.stdin.pause();
+//var colors = require('colors');
 
 var wordList = {};
 var nextWordList = [];
 var today = new Date();
-today.setHours(0,0,0,0); //Removes all times from new date
-console.log(today);
+today.setHours(0,0,0,0); //Remove all time from new date
 
 fs.readFile('baseWords.json', function(err, data) {
   if (err) throw err;
@@ -17,19 +13,26 @@ fs.readFile('baseWords.json', function(err, data) {
 });
 
 var loopWords = function(json) {
-  for (i=0; i<json.wordList.length; i++) {
-    var word = json.wordList[i];
-    if (!word.nextTestDate) { word.nextTestDate = today; } // first time through might not have a next test date
+  for (i=0; i<json.length; i++) {
+    var word = json[i];
+    if (!word.nextTestDate) { word.nextTestDate = today; } // if its a new word do a test today
+    if (!word.prevTestDate) { word.prevTestDate = today; }
+    if (!word.testHistory) { testHistory = []; } else { testHistory = word.testHistory; }
 
     var nextTestDate = new Date(word.nextTestDate); //convert to js date type
 
     if (nextTestDate <= today) {
-      //lets pretend they test and put 4 days for next test date
+      //lets pretend they test and we calculate 4 days for next test date
       nextTestDate.setDate(today.getDate() + 4);
-      nextWordList.push({ "english" : word.english, "spanish" : word.spanish, "nextTestDate" : nextTestDate});
+      console.log("You tested: 4 for the word " + word.english);
+      var testScore = {};
+      testScore.date = today;
+      testScore.score = 4;
+      testHistory.push(testScore);
+      nextWordList.push({ "english" : word.english, "spanish" : word.spanish, "nextTestDate" : nextTestDate, "prevTestDate" : today, "testHistory" : testHistory });
 
     } else { // just push it along if not due for test yet
-      nextWordList.push({ "english" : word.english, "spanish" : word.spanish, "nextTestDate" : word.nextTestDate});
+      nextWordList.push({ "english" : word.english, "spanish" : word.spanish, "nextTestDate" : word.nextTestDate, "prevTestDate" : word.prevTestDate, "testHistory" : testHistory});
     }
   }
   writeFile();

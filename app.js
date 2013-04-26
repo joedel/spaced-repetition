52710eb2f87612bd531290ec55f7c58a1e4f2c36
yojoe <joeleedel@gmail.com>
2013-04-26 01:51:26 -0400

    code reorg

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var readline = require('readline');
 
 var wordList = {};
-var nextWordList = [];
-var todaysWordList = [];
+var futureQuizList = [];
+var todaysQuizList = [];
 
 var today = new Date();
 today.setHours(0,0,0,0);
@@ -15,13 +15,35 @@ console.log("2. If you correctly knew the word by the time the answer was shown"
 console.log("3. If you had trouble or did not know the word at all");
 console.log("Get ready...");
 
+//You can change the input file here
 fs.readFile('baseWords.json', function(err, data) {
   if (err) throw err;
   wordList = JSON.parse(data);
-  todaysQuizWords(wordList);
+  buildQuizList(wordList);
 });
 
-function quizWord(number, wordList) {
+function buildQuizList(wordList) {
+  for (i=0; i<wordList.length; i++) {
+    var word = wordList[i];
+    if (!word.nextTestDate) { word.nextTestDate = today; } // first time through might not have a next test date, so do it today
+    if (!word.prevTestDate) { word.prevTestDate = today; }
+    if (!word.testHistory) { testHistory = []; } else { testHistory = word.testHistory; }
+    var nextTestDate = new Date(word.nextTestDate); //convert to js date type
+
+    if (nextTestDate <= today) {
+      todaysQuizList.push({ "word1" : word.word1, "word2" : word.word2, "nextTestDate" : nextTestDate, "prevTestDate" : word.prevTestDate, "testHistory" : testHistory });
+    } else { // just push it to the list to be saved if not due yet
+      futureQuizList.push({ "word1" : word.word1, "word2" : word.word2, "nextTestDate" : word.nextTestDate, "prevTestDate" : word.prevTestDate, "testHistory" : testHistory});
+    }
+  }
+}
+
+//Start quiz
+setTimeout(function() {
+   showQuizItem(0, todaysQuizList);
+ }, 3000);
+
+function showQuizItem(number, wordList) {
   if (!wordList[number]) {
     console.log("No more words available today.");
     writeFile();
@@ -30,13 +52,30 @@ function quizWord(number, wordList) {
     console.log("Spanish: " + currentWord.word2);
     setTimeout(function() {
       console.log("Correct Answer: " + currentWord.word1);
-      getTestResult(number, "Enter 1, 2 or 3: ");
+      askQuizResult(number, "Enter 1, 2 or 3: ");
     }, 4000);
   }
 }
 
+function askQuizResult(number, question) {
+  var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  rl.question(question, function(result) {
+        result = parseInt(result, 10);
+        if (result >= 1 && result <= 3) {
+            recordResults(number, result);
+            rl.close();
+        } else {
+            rl.close();
+            askQuizResult(number, question);
+        }
+  });
+}
+
 function recordResults(number, result) {
-  var wordToPush = todaysWordList[number];
+  var wordToPush = todaysQuizList[number];
   var prevTestDate = new Date(wordToPush.prevTestDate); //convert to js date
   var lastQuizInterval = 1;
   var nextQuizDate = new Date();
@@ -66,62 +105,22 @@ function recordResults(number, result) {
     //They did not know the word at all, show the word again tomorrow, or today if they run the program again.
     // nextQuizDate.setDate(nextQuizDate.getDate()); // default is today
   } else {
-    console.log("Something went wrong while recording results.");
+    console.log("Something went wrong while recording the result.");
   }
   //Push to next quiz word list
   wordToPush.nextTestDate = nextQuizDate;
   wordToPush.prevTestDate = today;
   wordToPush.testHistory.push({"date" : today, "score" : result});
-  nextWordList.push({ "word1" : wordToPush.word1, "word2" : wordToPush.word2, "nextTestDate" : wordToPush.nextTestDate, "prevTestDate" : wordToPush.prevTestDate, "testHistory" : wordToPush.testHistory});
+  futureQuizList.push({ "word1" : wordToPush.word1, "word2" : wordToPush.word2, "nextTestDate" : wordToPush.nextTestDate, "prevTestDate" : wordToPush.prevTestDate, "testHistory" : wordToPush.testHistory});
 
   //kick off the next question
   var nextWord = number + 1;
-  quizWord(nextWord, todaysWordList);
+  showQuizItem(nextWord, todaysQuizList);
 }
-
-function getTestResult(number, question) {
-  var rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-  rl.question(question, function(result) {
-        result = parseInt(result, 10);
-        if (result >= 1 && result <= 3) {
-            recordResults(number, result);
-            rl.close();
-        } else {
-            rl.close();
-            getTestResult(number, question);
-        }
-  });
-}
-
-function todaysQuizWords(wordList) {
-  for (i=0; i<wordList.length; i++) {
-    var word = wordList[i];
-    if (!word.nextTestDate) { word.nextTestDate = today; } // first time through might not have a next test date, so do it today
-    if (!word.prevTestDate) { word.prevTestDate = today; }
-    if (!word.testHistory) { testHistory = []; } else { testHistory = word.testHistory; }
-    var nextTestDate = new Date(word.nextTestDate); //convert to js date type
-
-    if (nextTestDate <= today) {
-      // add it to the list of todays test list
-      todaysWordList.push({ "word1" : word.word1, "word2" : word.word2, "nextTestDate" : nextTestDate, "prevTestDate" : word.prevTestDate, "testHistory" : testHistory });
-
-    } else { // just push it to the list to be saved if not due yet
-      nextWordList.push({ "word1" : word.word1, "word2" : word.word2, "nextTestDate" : word.nextTestDate, "prevTestDate" : word.prevTestDate, "testHistory" : testHistory});
-    }
-  }
-}
-
-//Start the quiz
-setTimeout(function() {
-   quizWord(0, todaysWordList);
- }, 3000);
 
 var writeFile = function() {
-  fs.writeFile('baseWords.json', JSON.stringify(nextWordList, null, 2), function(err) {
+  fs.writeFile('baseWords.json', JSON.stringify(futureQuizList, null, 2), function(err) {
     if (err) throw err;
-    console.log("Overwrite of word file complete.");
+    console.log("Results saved to file.");
   });
 };
